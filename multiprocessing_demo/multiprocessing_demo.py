d07eb30fc5af90ba96e68af4d2dcacf6f808ed40
Samuel Berston <samuelberston@gmail.com>
2023-12-06 23:51:54 -0800

    multiple processes demo

--- multiprocessing_demo/multiprocessing_demo.py
+++ multiprocessing_demo/multiprocessing_demo.py
@@ -1,10 +1,29 @@
-from multiprocessing import Pool
+from multiprocessing import Pool, Process
+import os
+
+
+def printProcessMetadata(processName: str) -> None:
+    print('process name:', processName)
+    print('module name:', __name__)
+    print('parent process:', os.getppid())
+    print('process id:', os.getpid())
+    print('\n')
 
 def printMessage(message: str) -> None:
+    printProcessMetadata()
     print(message)
 
-
 if __name__ == '__main__':  
-    p = Pool(5)
-    print(p.map(printMessage, ["Message 1", "Message 2", "Message 3"]))
+    # p = Pool(5)
+    # print(p.map(printMessage, ["Message 1", "Message 2", "Message 3"]))
+
+    process1 = Process(target=printProcessMetadata, args=('Process 1',))
+    process2 = Process(target=printProcessMetadata, args=('Process 2',))
+    process3 = Process(target=printProcessMetadata, args=('Process 3',))
+    process1.start()
+    process1.join()
+    process2.start()
+    process2.join()
+    process3.start()
+    process3.join()
 
