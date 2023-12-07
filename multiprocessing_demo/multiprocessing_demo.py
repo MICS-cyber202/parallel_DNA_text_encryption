c9c45912f598cd325b6b1d6b27456be17701ae0b
Samuel Berston <samuelberston@gmail.com>
2023-12-07 00:07:37 -0800

    add sleep to demonstrate parallel processing

--- multiprocessing_demo/multiprocessing_demo.py
+++ multiprocessing_demo/multiprocessing_demo.py
@@ -1,8 +1,10 @@
 from multiprocessing import Pool, Process
 import os
+import time
 
 
 def printProcessMetadata(processName: str) -> None:
+    time.sleep(5)
     print('process name:', processName)
     print('module name:', __name__)
     print('parent process:', os.getppid())
@@ -13,18 +15,22 @@ def printMessage(message: str) -> None:
     printProcessMetadata()
     print(message)
 
+
+
+
 if __name__ == '__main__':  
     # p = Pool(5)
     # print(p.map(printMessage, ["Message 1", "Message 2", "Message 3"]))
+    
+    print("CPU count: ", os.cpu_count())  
 
     process1 = Process(target=printProcessMetadata, args=('Process 1',))
     process2 = Process(target=printProcessMetadata, args=('Process 2',))
     process3 = Process(target=printProcessMetadata, args=('Process 3',))
     process1.start()
-    process1.join()
     process2.start()
-    process2.join()
     process3.start()
+    process1.join()
+    process2.join()
     process3.join()
 
-print("CPU count: ", os.cpu_count())
