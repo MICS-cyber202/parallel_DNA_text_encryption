a824a500d2aacf7e798bc2deb9a33df8e11b29ce
Samuel Berston <samuelberston@gmail.com>
2023-12-08 19:18:53 -0800

    add Callable type to multiprocessing_service method signature and start working on the chunk_plaintext function

--- text_encryption_service/multiprocessing_service.py
+++ text_encryption_service/multiprocessing_service.py
@@ -1,5 +1,5 @@
 # Requirements:
-# - this service will be a HOF that takes as arguments the encryption algorithm, the plaintext, & the X of processes to run
+# - this service will be a HOF that takes as arguments the encryption algorithm, the plaintext, the key, & the X of processes to run
 # - the service should have a performance analysis function which times the total encryption time
 # - the service should have a logging function which writes the results to a CSV for further analysis
 # 
@@ -9,9 +9,19 @@
 # 3) run X Processes concurrently, making sure to time them
 # 4) write the resulting time to the CSV file, along with the arguments metadata
 
-def multiprocessing_service(encryption_algorithm: function, plaintext: str, process_count: int) -> None:
+from typing import Callable
+
+def multiprocessing_service(encryption_algorithm: Callable[[str, str], str], plaintext: str, key: str, process_count: int) -> None:
     # Step 1: splice the plaintext into equal chunks (mod process_count) and store in an array
-    # Step 2: load X Process objects into an array with the target = encryption algorithm and the argument = the plaintext chunk
+    # Step 2: load X Process objects into an array with the target = encryption algorithm and the argument = the plaintext chunk and the key
     # Step 3: run X Processes concurrently, making sure to time them
     # Step 4: write the resulting time to the CSV file, along with the arguments metadata
     return 
+
+# helper functions
+def chunk_plaintext(plaintext: str, process_count: int) -> str:
+    chunks = [] * process_count
+    print(chunks)
+    return    
+
+print(chunk_plaintext("abcdefgh", 8))    
