5e37fec5ae46291f07647554cf48334c87f2b38e
Samuel Berston <samuelberston@gmail.com>
2023-12-08 19:09:49 -0800

    requirements for the multiprocessing service

---
+++ multiprocessing_service/multiprocessing_service.py
@@ -0,0 +1,10 @@
+# Requirements:
+# - this service will be a HOF that takes as arguments the encryption algorithm, the plaintext, & the X of processes to run
+# - the service should have a performance analysis function which times the total encryption time
+# - the service should have a logging function which writes the results to a CSV for further analysis
+# 
+# Steps:
+# 1) splice the plaintext into X equal chunks (mod X) and store them in an array of length X
+# 2) load X Process objects into an array with the target = encryption algorithm and the argument = the plaintext chunk
+# 3) run X Processes concurrently, making sure to time them
+# 4) write the resulting time to the CSV file, along with the arguments metadata
